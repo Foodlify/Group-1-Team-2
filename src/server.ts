855748f5aa464748d75e6952280278cc9721a09d
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2026-05-12 12:41:13 +0300

    Integrate logger for database connection and server start
    Replaced console logs with logger for better logging management.

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,28 @@
 import app from "./app";
 import prisma from "./lib/prisma";
-import 'dotenv/config'
+import "dotenv/config";
+import { logger } from "./config/logger";
 
 async function startServer() {
   try {
     await prisma.$connect();
-    console.log("✅ Connected to DB (Prisma)");
+    logger.info("Connected to DB (Prisma)");
+    //console.log("✅ Connected to DB (Prisma)");
 
     app.listen(3000, () => {
-      console.log("🚀 Server running on port 3000");
+      // console.log("🚀 Server running on port 3000");
+      logger.info("Server running", {
+        port: 3000,
+        env: process.env.NODE_ENV || "development",
+      });
     });
-    
   } catch (err) {
-    console.error("❌ Failed to connect to DB", err);
+    // console.error("❌ Failed to connect to DB", err);
+    logger.error("Failed to connect to DB", {
+      error: (err as Error).message,
+      stack: (err as Error).stack,
+    });
+
     process.exit(1);
   }
 }
