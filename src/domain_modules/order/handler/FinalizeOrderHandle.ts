a5764d3802f2ef7c42a630d73c8d74092588ac0c
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2026-05-12 12:48:02 +0300

    Refactor FinalizeOrderHandler to improve clarity

--- src/domain_modules/order/handler/FinalizeOrderHandle.ts
+++ src/domain_modules/order/handler/FinalizeOrderHandle.ts
@@ -1,37 +1,54 @@
 import prisma from "../../../lib/prisma";
-import { CartStatus } from "@prisma/client";
+import { CartStatus, PaymentMethod } from "@prisma/client";
 import { OrderRequest } from "../../../types/OrderRequest";
 import { OrderResponse } from "../../../types/OrderResponse";
 import { OrderHandler } from "./orderHandler";
+import { logger } from "../../../config/logger";
 
+export class FinalizeOrderHandler extends OrderHandler {
+  async handle(
+    request: OrderRequest,
+    response: OrderResponse
+  ): Promise<OrderResponse> {
+    const order = await prisma.order.create({
+      data: {
+        customerId: response.customerId!,
+        restaurantId: request.restaurantId,
+        addressId: request.addressId,
+        phone: request.phone,
+        notes: request.notes,
+        paymentMethod: request.paymentMethod,
+        totalPrice: response.totalPrice!,
+      },
+    });
 
-export class FinalizeOrderHandler extends OrderHandler{
-   async handle(request: OrderRequest, response: OrderResponse): Promise<OrderResponse> {
-       const order = await prisma.order.create({data: {
-                customerId: response.customerId!,
-                restaurantId: request.restaurantId,
-                addressId: request.addressId,
-                phone: request.phone,
-                notes: request.notes,
-                paymentMethod: request.paymentMethod,
-                totalPrice: response.totalPrice!,
-    }});
+    await prisma.orderItem.createMany({
+      data: response.orderItems.map((item) => ({ ...item, orderId: order.id })),
+    });
 
-    await prisma.orderItem.createMany({data:response.orderItems.map(item=>({...item,orderId:order.id}))});
+    await prisma.transaction.create({
+      data: {
+        orderId: order.id,
+        customerId: response.customerId,
+        paymentMethod: request.paymentMethod,
+        shippingFee: 10,
+        totalAmount: response.totalPrice,
+      },
+    });
 
-
-    await prisma.transaction.create({data:{
-        orderId:order.id , 
-        customerId:response.customerId , 
-        paymentMethod:request.paymentMethod ,
-        shippingFee:10,
-        totalAmount:response.totalPrice,
-
-    }});
-
-    await prisma.cart.update({where:{id:request.cartId},data:{status:CartStatus.COMPLETED}});
+    await prisma.cart.update({
+      where: { id: request.cartId },
+      data: { status: CartStatus.COMPLETED },
+    });
 
     response.orderId = order.id;
+
+    logger.info("Order finalized successfully", {
+      orderId: order.id,
+      customerId: response.customerId,
+      totalPrice: response.totalPrice,
+      PaymentMethod: request.paymentMethod,
+    });
     return this.handleNext(request, response);
-   }
+  }
 }
