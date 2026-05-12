76f41968af3fdba4d24914c500c4ebf79c0d160f
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2026-05-12 13:56:56 +0300

    Replace createCashOrder with createOrder using handlers
    Refactor order creation process to use handler chain.

--- src/domain_modules/order/order.services.ts
+++ src/domain_modules/order/order.services.ts
@@ -1,75 +1,40 @@
-import * as CustomerServices from "../customer/customer.services";
-import * as CartServices from "../cart/cart.services";
-import * as orderRepo from "../order/order.repository";
-import * as AddressServices from "../address/address.service";
-import { CartNotFoundExeption } from "../../shared/exceptions/Cart.exception";
-import { checkMenuItemsExistAndStock } from "./order.helpers";
-import prisma from "../../lib/prisma";
-import { PaymentMethod } from "@prisma/client";
+
+import { OrderHandler } from "./handler/orderHandler";
+import { CartCheckHandler } from "./handler/CartLockCheckHandler";
+import { ValidateCustomerExistHandler } from "./handler/ValidateCustomerHandler";
+import { validateAddressHandler } from "./handler/ValidateAddressHandler";
+import { ItemsAvailabilityCheckHandler } from "./handler/ItemsAvailabilityCheckHandler";
+import { FinalizeOrderHandler } from "./handler/FinalizeOrderHandle";
+import { PaymentProcessHandler } from "./handler/PaymentProcessHandler";
+import { OrderRequest } from "../../types/OrderRequest";
+import { OrderResponse } from "../../types/OrderResponse";
+import { logger } from "../../config/logger";
 
 
-// export const createCashOrder = async (userId: number, body: any) => {
-    
-//     const customer = await CustomerServices.getCustomerByUserId(userId);
-//     const cart = await CartServices.viewCart(customer.id);
+export const createOrder = async (
+  request: OrderRequest
+): Promise<OrderResponse> => {
+  const chain = OrderHandler.processOrder(
+    new CartCheckHandler(),
+    new ValidateCustomerExistHandler(),
+    new validateAddressHandler(),
+    new ItemsAvailabilityCheckHandler(),
+    new FinalizeOrderHandler(),
+    new PaymentProcessHandler()
+  );
 
-//     if (!cart || !cart.items.length) {
-//         throw new CartNotFoundExeption();
-//     }
+  logger.info("Starting order creation process", {
+    userId: request.userId,
+    cartId: request.cartId,
+    PaymentMethod: request.paymentMethod,
+  });
 
-//     const orderId = await prisma.$transaction(async (tx) => {
+  const response = await chain.handle(request, {} as OrderResponse);
 
-//         // 4.1 تحقق من الـ stock
-//         const menuItemMap = await checkMenuItemsExistAndStock(cart.items, tx);
+  logger.info("Order creation process completed", {
+    orderId: response.orderId,
+    totalPrice: response.totalPrice,
+  });
 
-//         // 4.2 جيب أو إنشاء الـ address
-//         let addressId: number;
-
-//         if (body.address.type === "existing") {
-//             const address = await AddressServices.getCustomerAddressById(body.address.addressId, customer.id, tx);
-//             addressId = address.id;
-//         } else {
-//             const { type, ...addressData } = body.address;
-//             const newAddress = await orderRepo.createAddress({ customerId: customer.id, ...addressData }, tx);
-//             addressId = newAddress.id;
-//         }
-
-//         // 4.3 إنشاء الـ Order
-//         const order = await orderRepo.createOrder({
-//             customerId: customer.id,
-//             restaurantId: cart.restaurantId,
-//             addressId,
-//             phone: body.phone,
-//             totalPrice: cart.totalPrice,
-//             paymentMethod: PaymentMethod.CASH,
-//             notes: body.notes,
-//         }, tx);
-
-//         // 4.4 إنشاء الـ OrderItems
-//         const orderItems = cart.items.map((item: any) => {
-//             const menuItem = menuItemMap.get(item.productId);
-//             return {
-//                 orderId: order.id,
-//                 menuItemId: item.productId,
-//                 itemName: menuItem!.itemName,
-//                 price: menuItem!.price,
-//                 quantity: item.quantity,
-//                 itemTotal: item.itemTotal,
-//             };
-//         });
-//         await orderRepo.createOrderItems(orderItems, tx);
-
-//         // 4.5 قلل الـ stock
-//         await orderRepo.updateMenuItemStock(
-//             orderItems.map((item: any) => ({ id: item.menuItemId, quantity: item.quantity })),
-//             tx
-//         );
-
-//         // 4.6 قفل الـ cart
-//         await CartServices.lockCart(cart.id, tx);
-
-//         return order.id; 
-//     });
-
-//     return await orderRepo.getOrderById(orderId);
-// };
+  return response;
+};
