6c3b5172dfb57f4e127af3037e4432c91cbf3add
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2026-05-12 14:03:45 +0300

    Refactor order controller to include createOrder method

--- src/domain_modules/order/order.controller.ts
+++ src/domain_modules/order/order.controller.ts
@@ -1,6 +1,26 @@
+import { OrderRequest } from "../../types/OrderRequest";
 import { asyncHandler } from "../../utils/asyncHandler";
-import { Request, Response } from "express";
-import * as orderService from "./order.services";
-import { sendSucess,sendError } from "../../utils/response";
-import { StatusCodes} from 'http-status-codes';
+import { Request, Response, NextFunction } from "express";
+import * as OrderServices from "./order.services";
+import { sendSucess } from "../../utils/response";
+import { StatusCodes } from "http-status-codes";
 
+export const createOrder = asyncHandler(async (req: Request, res: Response) => {
+  const request: OrderRequest = {
+    userId: req.userId as number,
+    cartId: req.body.cartId,
+    restaurantId: req.body.restaurantId,
+    addressId: req.body.addressId,
+    phone: req.body.phone,
+    notes: req.body.notes,
+    paymentMethod: req.body.paymentMethod,
+  };
+
+  const response = await OrderServices.createOrder(request);
+
+  sendSucess(res, {
+    message: "Order created successfully",
+    statusCode: StatusCodes.CREATED,
+    data: response,
+  });
+});
