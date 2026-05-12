9ae6a29f65fe27d9a2df1ec83a29004b29e8c638
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2026-05-12 14:04:32 +0300

    Update createOrder route to use createOrder controller

--- src/domain_modules/order/order.route.ts
+++ src/domain_modules/order/order.route.ts
@@ -4,8 +4,8 @@ import { validation } from "../../middlewares/validation.middleware";
 import * as orderSchemas from "./order.validation";
 import * as orderController from "./order.controller";
 
-
 const router = Router();
-// router.post("/createOrder" ,authenticate,validation(orderSchemas.createOrderSchema),orderController.createCashOrder);
+
+router.post("/createOrder", authenticate, validation(orderSchemas.createOrderSchema), orderController.createOrder);
 
 export default router;
